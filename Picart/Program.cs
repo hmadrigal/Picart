bb5344179977264370dd1f9cd93c53e14231c862
hmadrigal <hfmad@hotmail.com>
2023-07-20 17:40:10 -0600

    Adding command line parsing

---
+++ Picart/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.CommandLine;
+
+var inputOption = new Option<FileInfo>("Image file");
+var rootCommand = new RootCommand(description: "Convers a image to ASCII.")
+        { inputOption };
+
+rootCommand.SetHandler(async (FileInfo input) =>
+{
+    await Task.Yield();
+}, inputOption);
+await rootCommand.InvokeAsync(args);
+
+Console.WriteLine("Hello, World!");
